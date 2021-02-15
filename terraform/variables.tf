14ef0a6b01eca57f3bdff98e5e5df7e5c0cb1708
dmnbars <dmnbars@gmail.com>
2021-02-15 16:44:34 +0300

    конфигурация инфраструктуры разбита на файлы

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,3 +31,11 @@ variable app_instance_count {
   description = "count for reddit-app instance"
   default     = 1
 }
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
