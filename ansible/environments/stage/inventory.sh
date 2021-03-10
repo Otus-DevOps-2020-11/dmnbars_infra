51a4c9849bba309e1c68c17ebe06ea0ae5b79d3f
dmnbars <dmnbars@gmail.com>
2021-03-10 21:07:04 +0300

    сдиректория ansible приведена к новому виду

--- ansible/environments/stage/inventory.sh
+++ ansible/environments/stage/inventory.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scriptPath="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
-python $scriptPath/../../inventory_terraform.py stage
+python $scriptPath/../../old/inventory_terraform.py stage
